f89ebdd3768856e4be13645e5a2cc23e99751e79
lin lin <linlin@lindeMacBook-Air.local>
2018-09-29 21:37:30 +0800

    modify

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 /**
  * Created by Administrator on 2018/8/28.
  */
-// var apiHost = 'http://rap2api.taobao.org/app/mock/84859'
-var apiHost = 'http://106.37.196.133:8000'
+var apiHost = 'http://rap2api.taobao.org/app/mock/84859'
+    // var apiHost = 'http://106.37.196.133:8000'
