2d1ad37b2201f4a007bacd9a0902caf2c8ab400e
yixinzheng <459249363@qq.com>
2018-10-28 14:06:23 +0800

    api

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
 /**
  * Created by Administrator on 2018/8/28.
  */
-var apiHost = 'http://rap2api.taobao.org/app/mock/84859'
-//     var apiHost = 'http://106.37.196.133:8000'
+// var apiHost = 'http://rap2api.taobao.org/app/mock/84859'
+var apiHost = 'http://106.37.196.133:8000'
+
+axios.interceptors.request.use(
+    config => {
+        if (sessionStorage.token) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
+            config.headers.Authorization = 'Token ' + `${sessionStorage.token}`
+        } else {
+            if (location.href.indexOf('login') < 0 && location.href.indexOf('regist') < 0) {//判断在登录/注册页中
+                alert('登录已过期,请重新登录!')
+                location.href = '/html/login.html'
+            }
+        }
+        return config
+    },
+    err => {
+        return Promise.reject(err)
+    })
